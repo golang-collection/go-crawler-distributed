1a12b2e626e8de153c01a48ef898f3c450217126
super <supeng842499467@gmail.com>
2020-04-05 19:14:36 +0800

    rpc-support
    rpc-support

---
+++ rpcsupport/rpc.go
@@ -0,0 +1,41 @@
+package rpcsupport
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+func ServeRpc(host string,
+	service interface{}) error {
+	err := rpc.Register(service)
+	if err != nil{
+		return err
+	}
+
+	listener, err := net.Listen("tcp", host)
+	if err != nil{
+		return err
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil{
+			log.Printf("error %v", err)
+			continue
+		}
+		go jsonrpc.ServeConn(conn)
+	}
+	return nil
+}
+
+func NewClient(host string) (*rpc.Client, error) {
+	conn, err := net.Dial("tcp", ":1234")
+	if err != nil{
+		return nil, err
+	}
+
+	client := jsonrpc.NewClient(conn)
+	return client, nil
+}
