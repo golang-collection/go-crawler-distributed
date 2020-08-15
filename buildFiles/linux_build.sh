6760187105bcc707c1ae7843f5ae577e64ed5034
super <supeng842499467@gmail.com>
2020-08-15 09:27:05 +0800

    build

--- buildFiles/linux_build.sh
+++ buildFiles/linux_build.sh
@@ -10,8 +10,8 @@ crawerTags
 # 编译service可执行文件
 build_service() {
     rm -f ${ROOT_DIR}/deploy/linux/bin/
-    go build -o ${ROOT_DIR}/deploy/linux/bin/$1 ${ROOT_DIR}/crawer/$1.go
-    echo -e "\033[32m编译完成: \033[0m ${ROOT_DIR}/deploy/bin/"
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${ROOT_DIR}/deploy/linux/bin/$1 ${ROOT_DIR}/crawer/$1.go
+    echo -e "\033[32m编译完成: \033[0m ${ROOT_DIR}/deploy/linux/bin/"
 }
 
 # 执行编译service
--- buildFiles/win_build.sh
+++ buildFiles/win_build.sh
@@ -11,7 +11,7 @@ crawerTags
 build_service() {
     rm -f ${ROOT_DIR}/deploy/win/bin/
     CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ${ROOT_DIR}/deploy/win/bin/$1.exe ${ROOT_DIR}/crawer/$1.go
-    echo -e "\033[32m编译完成: \033[0m ${ROOT_DIR}/deploy/bin/"
+    echo -e "\033[32m编译完成: \033[0m ${ROOT_DIR}/deploy/win/bin/"
 }
 
 # 执行编译service
