7a8e1a53e298c2d187139984bcd8c46c4ce6310d
super <supeng842499467@gmail.com>
2020-08-31 18:59:05 +0800

    add deploy

--- deploy/buildScript/linux_build.sh
+++ deploy/buildScript/linux_build.sh
@@ -12,7 +12,7 @@ crawl_tags
 # 编译service可执行文件
 build_service() {
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o ${ROOT_DIR}/deploy/service/$1/bin/$1 ${ROOT_DIR}/service/$1/main.go
-    echo -e "\033[32m编译完成: \033[0m ${ROOT_DIR}/deploy/linux/bin/"
+    echo -e "\033[32m编译完成: \033[0m ${ROOT_DIR}/deploy/service/$1/bin/$1"
 }
 
 # 执行编译service
