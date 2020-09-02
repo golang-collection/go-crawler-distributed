789fa6cd9b2574ae37bce46641a1e7408a81b5cf
super <supeng842499467@gmail.com>
2020-09-02 09:47:54 +0800

    deploy

--- deploy/deploy/start-meituan-direct.sh
+++ deploy/deploy/start-meituan-direct.sh
@@ -2,20 +2,22 @@
 ROOT_DIR=/Users/super/develop/go-crawler-distributed
 
 services="
-cache
 storage_detail
 crawl_detail
 crawl_list
-crawl_tags
+crawl_urllist
 "
 
 cd ${ROOT_DIR}
 
 # 编译service可执行文件
 run_service() {
-    go run ${ROOT_DIR}/service/$1/main.go
+    go run ${ROOT_DIR}/service/meituan/$1/main.go
     echo -e "\033[32m启动完成: \033[0m $1"
 }
+
+go run ${ROOT_DIR}/service/elastic/main.go
+echo -e "\033[32m启动完成: \033[0m elastic"
 
 # 执行编译service
 for service in $services
