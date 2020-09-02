47af858d624aba8f6b5abc0de3735354039ab138
super <supeng842499467@gmail.com>
2020-09-02 10:35:36 +0800

    deploy

--- elastic/elasticTools/elasticTools.go
+++ elastic/elasticTools/elasticTools.go
@@ -1,10 +1,12 @@
 package elasticTools
 
 import (
+	"errors"
 	"github.com/olivere/elastic/v7"
 	"go-crawler-distributed/service/watchConfig"
 	"go-crawler-distributed/unifiedLog"
 	"go.uber.org/zap"
+	"time"
 )
 
 /**
@@ -17,11 +19,8 @@ var client *elastic.Client
 var logger = unifiedLog.GetLogger()
 
 func init() {
-	var err error
 	url, _ := watchConfig.GetElasticUrl()
-	client, err = elastic.NewClient(
-		elastic.SetURL(url),
-		elastic.SetSniff(false))
+	err := pingServer(url)
 	if err != nil{
 		logger.Error("elastic new client error", zap.Error(err))
 		panic(err)
@@ -31,4 +30,20 @@ func init() {
 
 func GetClient() *elastic.Client{
 	return client
+}
+
+// pingServer pings the http server to make sure the router is working.
+func pingServer(url string) error {
+	for i := 0; i < 10; i++ {
+		// Ping the server by sending a GET request to `/health`.
+		var err error
+		client, err = elastic.NewClient(
+			elastic.SetURL(url),
+			elastic.SetSniff(false))
+		if err == nil{
+			return nil
+		}
+		time.Sleep(time.Second)
+	}
+	return errors.New("cannot connect to the elastic server")
 }
